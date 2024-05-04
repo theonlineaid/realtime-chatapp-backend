10569db2d4e80ebe858025d689e5d413877ab264
theonlineaid <nkomorfaruk@gmail.com>
2024-05-04 16:40:10 +0600

    Prevent cors error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 // import connectDB from './db/connectDB.js';
+import cors from 'cors';
 
 import authRoutes from './routes/authRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
@@ -14,6 +15,7 @@ console.log(dotenv.config().parsed.PORT);
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(cookieParser());
 
 
